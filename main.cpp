0de4d93fd07eaf7705e7c14334021192d95f8125
ssp2018devops <36333971+ssp2018devops@users.noreply.github.com>
2018-02-11 16:17:29 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+class ThisIsAClass
+{
+  public:
+    void method() const;
+}
+
+void ThisIsAClass::method() const
+{
+  // hello
+}
+
+int main()
+{
+  ThisIsAClass a;
+  a.method();
+  
+  return 0;
+}
